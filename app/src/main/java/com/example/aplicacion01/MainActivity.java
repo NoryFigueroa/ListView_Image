e9de72d643f2bc01b27dfb783c68bc6cacdcf288
unknown <admin@IHR80PBF211.continental.edu.pe>
2020-01-11 13:16:49 -0500

    creacion de post en android

--- app/src/main/java/com/example/aplicacion01/MainActivity.java
+++ app/src/main/java/com/example/aplicacion01/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.aplicacion01;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.aplicacion01.adapters.ContactoAdaptador;
 import com.example.aplicacion01.helpers.QueueUtils;
@@ -21,12 +24,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnTipo = findViewById(R.id.btnTipo);
+        Button btnTipo2 = findViewById(R.id.btnTipo2);
+        btnTipo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Refrescar platos", Toast.LENGTH_SHORT).show();
+                items.clear();
+                Contacto.injectContactsFromCloud(queue, items, MainActivity.this, "limeno");
+            }
+        });
+
+        btnTipo2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "platos huaninos", Toast.LENGTH_SHORT).show();
+                items.clear();
+                Contacto.injectContactsFromCloud(queue, items, MainActivity.this, "huancaino");
+            }
+        });
+
+
+
+
         contactosList = findViewById(R.id.contactosList);
 
         queue = QueueUtils.getInstance(this.getApplicationContext());
         items = new ArrayList<>();
 
-        Contacto.injectContactsFromCloud(queue, items, this);
+        Contacto.injectContactsFromCloud(queue, items, this, "huancaino");
 
         contactoAdaptador = new ContactoAdaptador(this, items, queue.getImageLoader());
         contactosList.setAdapter(contactoAdaptador);
--- app/src/main/java/com/example/aplicacion01/models/Contacto.java
+++ app/src/main/java/com/example/aplicacion01/models/Contacto.java
@@ -43,7 +43,8 @@ public class Contacto {
     public static void sendRequestPOST(QueueUtils.QueueObject o, final MainActivity _interface) {
         String url = "http://rrojasen.alwaysdata.net/purchaseorders.json";
         url = "http://fipo.equisd.com/api/users/new.json";
-        /*url = "http://192.168.58.3:8056/api/users/new.json";*/
+        /*url = "https://congruous-gerbera.glitch.me/rooms.json/new.json"*/;
+        url = "https://congruous-gerbera.glitch.me/rooms/new.json";
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
                     @Override
@@ -94,8 +95,10 @@ public class Contacto {
 
     public static void injectContactsFromCloud(final QueueUtils.QueueObject o,
                                                final ArrayList<Contacto> contactos,
-                                               final MainActivity _interface) {
+                                               final MainActivity _interface,
+                                               final String tipo) {
         String url = "http://fipo.equisd.com/api/users.json";
+        url = "https://congruous-gerbera.glitch.me/rooms.json?tipo=" + tipo;
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
@@ -107,8 +110,8 @@ public class Contacto {
                                 JSONArray list = response.getJSONArray("objects");
                                 for (int i = 0; i < list.length(); i++) {
                                     JSONObject o = list.getJSONObject(i);
-                                    contactos.add(new Contacto(o.getString("first_name"),
-                                            o.getString("last_name"), ""));
+                                    contactos.add(new Contacto(o.getString("name"),
+                                            o.getString("descripcion"), ""));
                                 }
 
                             } catch (JSONException e) {
